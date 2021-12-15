0721d099de12c2445c69964645863ccb4dca5ac1
YaBear <theyabear@ya.ru>
2021-12-15 19:48:25 +0000

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+bottoken = os.environ['bottoken']
+IDbot = os.environ['IDbot']
+GameGuild = os.environ['GameGuild']
+
+settings = {
+ 'token': bottoken,
+ 'bot': 'Badgeria RPG',
+ 'id': IDbot,
+ 'prefix': '-',
+ 'level': '1',
+ 'gameguild': GameGuild
+ }
---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from config import settings
+bot = commands.Bot(command_prefix = settings['prefix'])  #Определение префикса из config.py
+
+@bot.event #Отладка, что бот запущен (в консоли)
+async def on_ready():
+    print("Готов к работе")
+    print(settings['level'])
+
+@bot.command() #Регистрация игрока
+async def Начать(ctx):
+ guild = ctx.message.guild
+ category = guild.get_channel(920739979972448307)
+ author = ctx.message.author.name+'-уровень-'+settings['level']
+ await category.create_text_channel(author)
+ await ctx.send(f'Чтож, начнем!')
+ 
+@bot.command() #Тест приветствия
+async def Привет(ctx):
+ author = ctx.message.author
+ await ctx.send(f'Привет, {author.mention}, Бог-Барсук рад встрече!')
+
+bot.run(settings['token']) #Запуск бота, финальная строка
